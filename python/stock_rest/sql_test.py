8541907322242552ec7c08147d71149935f5a9f9
hilockman <43579183@qq.com>
2018-06-22 02:18:05 +0800

    update

---
+++ python/stock_rest/sql_test.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+import tushare as ts
+
+df = ts.get_tick_data('600848', date='2014-12-22')
+engine = create_engine('mysql://root:root@127.0.0.1/stock?charset=utf8')
+
+#存入数据库
+df.to_sql('tick_data',engine)
+
+#追加数据到现有表
+#df.to_sql('tick_data',engine,if_exists='append')
